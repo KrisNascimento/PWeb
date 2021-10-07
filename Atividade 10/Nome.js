a063f139e101eef59e77ae9a0801baec5e733a3a
KrisNascimento <61701788+KrisNascimento@users.noreply.github.com>
2021-10-06 22:12:34 -0300

    Add files via upload

---
+++ Atividade 10/Nome.js
@@ -0,0 +1,13 @@
+var Aluno1 = new Object();
+Aluno1.ra = '0030481923043';
+Aluno1.nome = 'Kris Nascimento';
+
+var Aluno1 = {};
+Aluno1.ra = '0030481923043';
+Aluno1.nome = 'Kris Nascimento';
+
+var Aluno1 = {
+    ra: '0030481923043',
+    nome: 'Kris Nascimento'
+};
+alert("RA: " + Aluno1.ra + "\nAluna: " + Aluno1.nome);
